4284881c51372c510313a85ffad9ebd7af065b1a
Brinly <brinly@brinly.me>
2018-03-31 09:14:11 +1100

    bardockpro: Build ebtables

--- device.mk
+++ device.mk
@@ -179,6 +179,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service
 
+# IPv6
+PRODUCT_PACKAGES += \
+    ebtables \
+    ethertypes
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1
