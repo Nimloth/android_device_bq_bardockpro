0f9c86f052643ae3748a932cd7378bc47a9ecdc6
Brinly <brinly@brinly.me>
2018-04-03 19:42:23 +1000

    bardock: Add more audio configs

--- device.mk
+++ device.mk
@@ -117,6 +117,8 @@ PRODUCT_COPY_FILES += \
 
 # Use custom mixer paths
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/configs/audio/audio_platform_info_extcodec:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_extcodec \
     $(LOCAL_PATH)/configs/audio/mixer_paths_mtp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_mtp_no_hifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_mtp_no_hifi.xml
 
