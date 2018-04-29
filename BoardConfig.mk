c6f2eff18b9c6da6bc13bf57411995ada49ca539
Rashed Abdel-Tawab <rashed@linux.com>
2018-04-28 17:47:08 -0700

    bardockpro: Build wlan driver into kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,8 +191,6 @@ WIFI_DRIVER_FW_PATH_STA := "sta"
 WIFI_DRIVER_FW_PATH_AP  := "ap"
 WIFI_DRIVER_FW_PATH_P2P := "p2p"
 TARGET_USES_WCNSS_CTRL  := true
-WIFI_DRIVER_MODULE_PATH := "/system/vendor/lib/modules/wlan.ko"
-WIFI_DRIVER_MODULE_NAME := "wlan"
 PRODUCT_VENDOR_MOVE_ENABLED := true
 
 # Inherit from the proprietary version
